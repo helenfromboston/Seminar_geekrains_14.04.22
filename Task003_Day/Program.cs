a160d16db555db9da5b39ebb3ca5c8fe5013c17e
helenfromboston <helenfromboston@gmail.com>
2022-04-14 11:31:55 +0300

    Added Tasks 000,001,003

---
+++ Task003_Day/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Write("Введите номер дня недели: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number == 1)
+{
+    Console.Write("Понедельник");
+}
+if (number == 2)
+{
+    Console.Write("Вторник");
+}
+if (number == 3)
+{
+    Console.Write("Среда");
+}
+if (number == 4)
+{
+    Console.Write("Четверг");
+}
+if (number == 5)
+{
+    Console.Write("Пятница");
+}
+if (number == 6)
+{
+    Console.Write("Суббота");
+}
+if (number == 7)
+{
+    Console.Write("Воскресенье");
+}
+if (number > 7)
+{
+    Console.Write("Недействительный номер");
+}
